29fde5e5c96b83f3dd1461898f46437842b6666c
Ivan-Kryvaruchka_epam <166821521+IvanKryvaruchka@users.noreply.github.com>
2024-10-04 15:48:39 +0300

    Add configuration feature to limit products shown on Product page
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/IvanKryvaruchka/MyTestProejct?shareId=XXXX-XXXX-XXXX-XXXX).

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 using WebAPI.Data;
@@ -10,14 +11,18 @@ namespace WebAPI.Controllers
     public class ProductsController : Controller
     {
         private readonly NorthwindContext _context;
+        private readonly IConfiguration _configuration;
 
-        public ProductsController(NorthwindContext context)
+        public ProductsController(NorthwindContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
         public async Task<IActionResult> Index()
         {
+            var maxProductsToShow = _configuration.GetValue<int>("MaxProductsToShow");
+
             var products = await _context.Products
                 .Select(p => new Product
                 {
@@ -35,6 +40,11 @@ namespace WebAPI.Controllers
                     Discontinued = p.Discontinued
                 })
                 .ToListAsync();
+
+            if (maxProductsToShow > 0)
+            {
+                products = products.Take(maxProductsToShow).ToList();
+            }
 
             return View(products);
         }
